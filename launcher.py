fd897cf5238820c9e045e6673bd6276213f830dd
pxsocs <cryptoblotter@alphazeta.io>
2019-09-06 11:11:27 -0300

    Small Fix on finding Dojo info

--- launcher.py
+++ launcher.py
@@ -1,11 +1,16 @@
 import PySimpleGUI as sg
 # Main Windows Launcher for APP
-
 layout = [[
     sg.Text('Welcome to the WARden', font='Courier 12', text_color='blue')
-], [sg.OK(), sg.Cancel()]]
+], [sg.Button('Launch WARden Server')], [sg.Button('Check for Updates')],
+          [sg.InputText()], [sg.Button('Exit')]]
+
 # Create the Window
-window = sg.Window('The WARden', layout)
+window = sg.Window('The WARden').Layout(layout)
+
+# Readers on the Window
+button_launch = window.Read()
+
 # Event Loop to process "events"
 while True:
     event, values = window.Read()
